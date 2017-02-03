9a1007db50a7e19d9013a18dcfa2c8a725eef858
gosher <macciak@gmail.com>
2017-02-03 14:41:04 +0000

    add data validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ActiveRecord::Base
     mount_uploader :avatar, AvatarUploader
     has_many :reviews
+    
+    validates :name, :address , :phone, :website , :avatar , presence: true
+    validates :phone, format: { with: /\A\(\d{2}\)\d{2}-\d{2}-\d{2}-\d{2}-\d{2}\z/, 
+        message: "Must be in the format (xx)xx-xx-xx-xx-xx"}
+    validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+        message: "Must start with http:// or https://"}
 end
